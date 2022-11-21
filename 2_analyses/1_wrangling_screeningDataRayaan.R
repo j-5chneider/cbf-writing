37c81b0a13abcea422a0316b98d3ec97b4c3707d
j_5chneider <j.schn@posteo.de>
2022-11-21 16:51:09 +0100

    cleaning data: export from rayaan

--- 2_analyses/1_wrangling_screeningDataRayaan.R
+++ 2_analyses/1_wrangling_screeningDataRayaan.R
@@ -1,27 +1,31 @@
 library(tidyverse)
 
-art_csv <- rio::import(file="../data/articles.csv")
+art_csv <- rio::import(file="data/articles.csv")
 
 art_csv <- art_csv %>%
-  dplyr::mutate(excl_reason = str_extract(.$note, "RAYYAN-EXCLUSION-REASONS:.*?(\\||$)"),
-                labels = str_extract(.$note, "RAYYAN-LABELS:.*?(\\||$)"),
-                decision_j = str_extract(.$note, "juergen.schneider\"\"=>\"\".*?\"\""),
-                decision_s = str_extract(.$note, "Salome\"\"=>\"\".*?\"\""))%>%
+  dplyr::mutate(excl_reason = str_extract(.$notes, "RAYYAN-EXCLUSION-REASONS:.*?(\\||$)"), # extract exclusion reasons
+                labels = str_extract(.$notes, "RAYYAN-LABELS:.*?(\\||$)"),       # extract labels coded
+                decision_j = str_extract(.$notes, "Jürgen\"\"=>\"\".*?\"\""),    # extract decision from coder 1
+                decision_s = str_extract(.$notes, "Salome\"\"=>\"\".*?\"\""))%>% # extract decision from coder 2
+  # remove prefixes or recode
   dplyr::mutate(excl_reason = str_remove_all(excl_reason, "RAYYAN-EXCLUSION-REASONS:\\s"),
                 labels = str_remove_all(labels, "RAYYAN-LABELS:\\s|\\s\\|"),
-                decision_j = case_when(                                                    # Diese Zeilen für die Neue Variable
-                  str_detect(decision_j, "Excluded") ~ "Excluded",           # ebenfalls kopieren und
-                  str_detect(decision_j, "Included") ~ "Included",           # den neuen Variablennamen
-                  str_detect(decision_j, "Maybe") ~ "Maybe"),                # einfügen
-                decision_s = case_when(                                                    # Diese Zeilen für die Neue Variable
-                  str_detect(decision_s, "Excluded") ~ "Excluded",           # ebenfalls kopieren und
-                  str_detect(decision_s, "Included") ~ "Included",           # den neuen Variablennamen
-                  str_detect(decision_s, "Maybe") ~ "Maybe")                 # einfügen
+                decision_j = case_when(                                      
+                  str_detect(decision_j, "Excluded") ~ "Excluded",           
+                  str_detect(decision_j, "Included") ~ "Included",           
+                  str_detect(decision_j, "Maybe") ~ "Maybe"),                
+                decision_s = case_when(                                      
+                  str_detect(decision_s, "Excluded") ~ "Excluded",           
+                  str_detect(decision_s, "Included") ~ "Included",           
+                  str_detect(decision_s, "Maybe") ~ "Maybe")                 
   )
 
 
 
-
+# crosstab of coding results
 table(art_csv$decision_j, art_csv$decision_s)
 table(art_csv$decision_j)
 table(art_csv$decision_s)
+
+# write table with clean data set to data folder
+write_csv(art_csv, file = "data/articles_clean.csv")
